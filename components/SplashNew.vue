fa66c05000795b4f47fde9cab24517a545d88a3e
Lionel Mann <lionelmann@gmail.com>
2020-11-16 15:52:38 -0500

    changed trailer link

--- components/SplashNew.vue
+++ components/SplashNew.vue
@@ -36,7 +36,7 @@
       <div slot="body">
         <div style="padding:56.25% 0 0 0;position:relative;">
           <iframe
-            src="https://player.vimeo.com/video/395205887?title=0&byline=0&portrait=0&autoplay=1"
+            src="https://player.vimeo.com/video/445245232?title=0&byline=0&portrait=0&autoplay=1"
             style="position:absolute;top:0;left:0;width:100%;height:100%;"
             frameborder="0"
             allow="autoplay; fullscreen"
